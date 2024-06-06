b336ae93c1a61f08a3dd552f0c9b8a42b70b31f5
Lucas Gominho <lcpg@cesar.school>
2024-06-06 16:54:48 -0300

    Corrigindo logica do login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -14,18 +14,20 @@ const Login = ({ onLogin }) => {
 
     try {
       const response = await api.post('api/login', { email, senha });
-      console.log(response.result);
-      if (response.result) {
+      if (response.data.result) {
         onLogin(response.data.token, email);
         setMensagem('Login bem-sucedido');
-        navigate.push('/');
+        setTimeout(() => {
+          navigate('/');
+        }, 3000); // Redireciona após 3 segundos
       } else {
         setMensagem('Login ou senha incorretos');
       }
     } catch (error) {
       setMensagem('Erro ao efetuar login');
+      console.error('Erro ao efetuar login:', error);
     }
-  };
+  };  
 
   return (
     <div className="login-container">
